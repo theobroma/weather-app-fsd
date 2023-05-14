2cfae08c41bc7f551c01c272740bf377204bfa42
Alexandr Siryj <theobroma222@gmail.com>
2023-05-14 04:30:17 +0300

    ForecastResponse

--- src/types/z.forecast.ts
+++ src/types/z.forecast.ts
@@ -1,5 +1,26 @@
 import { z } from 'zod';
 
-export type Keep = {
-  keep: string;
-};
+import { CurrentWeatherSchema } from './z.current';
+import { AstroSchema, DaySchema, HourSchema } from './z.general';
+import { LocationSchema } from './z.location';
+
+export const ForecastDaySchema = z.object({
+  astro: AstroSchema,
+  date: z.string(), // "2023-05-14"
+  dateEpoch: z.number(), // 1684022400,
+  day: DaySchema,
+  hour: HourSchema,
+});
+
+// ================ Response ================
+
+export const ForecastResponseSchema = z.object({
+  current: CurrentWeatherSchema,
+  forecast: z.object({
+    forecastDay: z.array(ForecastDaySchema),
+  }),
+
+  location: LocationSchema,
+});
+
+export type ForecastResponseType = z.infer<typeof ForecastResponseSchema>;
--- src/types/z.general.ts
+++ src/types/z.general.ts
@@ -7,3 +7,79 @@ export const ConditionSchema = z.object({
 });
 
 export type ConditionType = z.infer<typeof ConditionSchema>;
+
+export const AstroSchema = z.object({
+  sunrise: z.string(),
+  sunset: z.string(),
+  moonrise: z.string(),
+  moonset: z.string(),
+  moonPhase: z.string(),
+  moonIllumination: z.string(),
+  isMoonUp: z.number(),
+  isSunUp: z.number(),
+});
+
+// export type AstroType = z.infer<typeof AstroSchema>;
+
+export const DaySchema = z.object({
+  maxtempC: z.number(),
+  maxtempF: z.number(),
+  mintempC: z.number(),
+  mintempF: z.number(),
+  avgtempC: z.number(),
+  avgtempF: z.number(),
+  maxwindMph: z.number(),
+  maxwindKph: z.number(),
+  totalprecipMm: z.number(),
+  totalprecipIn: z.number(),
+  totalsnowCm: z.number(),
+  avgvisKm: z.number(),
+  avgvisMiles: z.number(),
+  avghumidity: z.number(),
+  dailyWillItRain: z.number(),
+  dailyChanceOfRain: z.number(),
+  dailyWillItSnow: z.number(),
+  dailyChanceOfSnow: z.number(),
+  condition: ConditionSchema,
+  uv: z.number(),
+});
+
+// export type DayType = z.infer<typeof DaySchema>;
+
+export const HourSchema = z.object({
+  timeEpoch: z.number(),
+  time: z.string(),
+  tempC: z.number(),
+  tempF: z.number(),
+  isDay: z.number(),
+  condition: ConditionSchema,
+  windMph: z.number(),
+  windKph: z.number(),
+  windDegree: z.number(),
+  windDir: z.string(),
+  pressureMb: z.number(),
+  pressureIn: z.number(),
+  precipMm: z.number(),
+  precipIn: z.number(),
+  humidity: z.number(),
+  cloud: z.number(),
+  feelslikeC: z.number(),
+  feelslikeF: z.number(),
+  windchillC: z.number(),
+  windchillF: z.number(),
+  heatindexC: z.number(),
+  heatindexF: z.number(),
+  dewpointC: z.number(),
+  dewpointF: z.number(),
+  willItRain: z.number(),
+  chanceOfRain: z.number(),
+  willItSnow: z.number(),
+  chanceOfSnow: z.number(),
+  visKm: z.number(),
+  visMiles: z.number(),
+  gustMph: z.number(),
+  gustKph: z.number(),
+  uv: z.number(),
+});
+
+// export type HourType = z.infer<typeof HourSchema>;
