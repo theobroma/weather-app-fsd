9d16078ff9cbbf4429759cd3ef47a4bc88f65b0e
Alexandr Siryj <theobroma222@gmail.com>
2023-05-19 08:11:37 +0300

    debounced search

--- src/pages/home/home-search/home-search.tsx
+++ src/pages/home/home-search/home-search.tsx
@@ -1,10 +1,57 @@
-import { Grid, Box, Paper } from '@mui/material';
+import { ChangeEvent, useState } from 'react';
+import { useDebounce } from 'usehooks-ts';
+
+import SearchIcon from '@mui/icons-material/Search';
+import {
+  Grid,
+  Box,
+  Paper,
+  FormControl,
+  InputAdornment,
+  InputLabel,
+  OutlinedInput,
+} from '@mui/material';
 
 export const HomeSearch = () => {
+  const [value, setValue] = useState<string>('');
+  const debouncedValue = useDebounce<string>(value, 500);
+
+  // const handleChange = () => (event: React.ChangeEvent<HTMLInputElement>) => {
+  //   setSearchVal(event.currentTarget.value.trim());
+  // };
+
+  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setValue(event.target.value);
+  };
+
   return (
     <Grid item xs={12}>
       <Box mb={1}>
-        <Paper elevation={3}>HomeSearch</Paper>
+        <Paper elevation={3}>
+          <Box p={3}>
+            <FormControl fullWidth variant="outlined">
+              <InputLabel htmlFor="search-input">Search</InputLabel>
+              <OutlinedInput
+                // need for correct styles. Same text as in InputLabel
+                label="Search"
+                id="search-input"
+                value={value}
+                onChange={handleChange}
+                startAdornment={
+                  <InputAdornment position="start">
+                    <SearchIcon />
+                  </InputAdornment>
+                }
+              />
+            </FormControl>
+
+            {/* {searchData.length > 0 && (
+              <div className={classes.outputWrap}>
+                <SearchOutput searchData={searchData} onClick={onPlaceClick} />
+              </div>
+            )} */}
+          </Box>
+        </Paper>
       </Box>
     </Grid>
   );
